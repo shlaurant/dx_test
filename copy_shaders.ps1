5870eaba759a74e935c4e776bd7058509eead103
Laurant <shklaurant@gmail.com>
2023-01-27 11:48:10 +0900

    fixed copy_shaders.ps1 to froce copy

--- copy_shaders.ps1
+++ copy_shaders.ps1
@@ -1,2 +1,2 @@
-Copy-Item -Path "shader\*.cso" -Destination "cmake-build-debug\bin\shader" -Recurse
-Copy-Item -Path "resource\*" -Destination "cmake-build-debug\bin\resource" -Recurse
+Copy-Item -Path "shader\*.cso" -Destination "cmake-build-debug\bin\shader" -Recurse -force
+Copy-Item -Path "resource\*" -Destination "cmake-build-debug\bin\resource" -Recurse -force
