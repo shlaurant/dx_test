bfced8b26ba912182cc3e34d09f7bb905a7553e8
Laurant <shklaurant@gmail.com>
2023-01-27 16:33:29 +0900

    improved build chain

--- copy_shaders.ps1
+++ copy_shaders.ps1
@@ -1,5 +1,5 @@
-Copy-Item -Path "shader\*.cso" -Destination "cmake-build-debug-vs-64\bin\shader" -Recurse -force
+Copy-Item -Path "shader\debug\*.cso" -Destination "cmake-build-debug-vs-64\bin\shader" -Recurse -force
 Copy-Item -Path "resource\*" -Destination "cmake-build-debug-vs-64\bin\resource" -Recurse -force
 
-Copy-Item -Path "shader\*.cso" -Destination "cmake-build-release-vs-64\bin\shader" -Recurse -force
+Copy-Item -Path "shader\release\*.cso" -Destination "cmake-build-release-vs-64\bin\shader" -Recurse -force
 Copy-Item -Path "resource\*" -Destination "cmake-build-release-vs-64\bin\resource" -Recurse -force
